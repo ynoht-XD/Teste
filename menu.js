f6c16692ad7203e9569e12d8476698435df8e8a2
Anthony Barroso <65836258+ynoht-XD@users.noreply.github.com>
2023-11-03 17:00:14 -0300

    plus JS

--- menu.js
+++ menu.js
@@ -2,3 +2,9 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('.img-cap-ios')
     header.classList.toggle('rolagem',window.scrollY > 0)
 })
+
+slider.oninput = changeFontSize;
+
+function changeFontSize() {
+    city.style.fontSize = this.value + "px";
+}
